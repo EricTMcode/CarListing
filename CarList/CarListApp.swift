6d259fa137e16d0f633fadc0f2339e4c64aef61f
DarkbartoTM <93444335+DarkbartoTM@users.noreply.github.com>
2022-10-07 13:28:23 +0200

    Initial Commit

---
+++ CarList/CarListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarListApp.swift
+//  CarList
+//
+//  Created by Eric on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
