bde3a66f62dd316f91cfee3dd8d79a0770763c90
DarkbartoTM <93444335+DarkbartoTM@users.noreply.github.com>
2022-10-07 14:34:42 +0200

    - add Pros and Cons data into example

--- CarList/Models/Car.swift
+++ CarList/Models/Car.swift
@@ -19,5 +19,5 @@ struct Car: Identifiable, Codable {
     var rating: Int
     var image: String
     
-    static let example = Car(consList: [""], customerPrice: 12550, make: "Land Rover", marketPrice: 14500, model: "Range Rover", prosList: [""], rating: 4, image: "Range_Rover")
+    static let example = Car(consList: ["You can heard the engine over children cry at the back","","You may lose this one if you divorce"], customerPrice: 12550, make: "Land Rover", marketPrice: 14500, model: "Range Rover", prosList: ["Your average business man car","Can bring the family home safely","The city must have"], rating: 4, image: "Range_Rover")
 }
